f9737da04c876754be4a7faec72820c40ceb7eb2
NaveedHafeez004 <144110314+NaveedHafeez004@users.noreply.github.com>
2023-11-15 14:00:51 +0530

    took speciality and doctor details from database

--- controller/doctorRoute.js
+++ controller/doctorRoute.js
@@ -1,74 +1,106 @@
-const express=require("express");
+// Backend doctor route
+const express = require("express");
 const doctorSchema = require("../model/doctorSchema");
-const mongoose=require("mongoose")
-const doctorRoute=express.Router();
+const mongoose = require("mongoose");
+const doctorRoute = express.Router();
 
-//http://localhost:4000/doctor/createDoctor
-doctorRoute.post("/createDoctor",(req, res)=>{
-    doctorSchema.create(req.body,(err,data)=>{
-        if (err) {
-            return err;
-        }
-        else {
-            res.json(data);
-        }
-    })
-})
-
-//http://localhost:4000/doctor/getDoctor
-doctorRoute.get("/getDoctor", async (req, res) => {
-    const {doctorId} = req.query;
-    if (!doctorId) {
-        return res.status(400).json({ error: "Please provide doctor ID" });
+// http://localhost:4000/doctor/createDoctor
+doctorRoute.post("/createDoctor", (req, res) => {
+  doctorSchema.create(req.body, (err, data) => {
+    if (err) {
+      return err;
+    } else {
+      res.json(data);
     }
-    try {
-        const doctors = await doctorSchema.find({ doctorId: doctorId });
-        if (doctors.length === 0) {
-            return res.status(404).json({ error: "No doctor found with the provided doctor id" });
-        }
-        res.status(200).json(doctors);
-    } catch (error) {
-        console.log("error:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+  });
 });
 
-//http://localhost:4000/doctor
-doctorRoute.get("/",(req,res)=>{
-    doctorSchema.find((err,data)=>{
-        if(err) {
-            return err;
-        }
-        else {
-            res.json(data);
-        }
-    })
-})
+// http://localhost:4000/doctor
+doctorRoute.get("/", (req, res) => {
+  doctorSchema.find((err, data) => {
+    if (err) {
+      return err;
+    } else {
+      res.json(data);
+    }
+  });
+});
 
-//http://localhost:4000/doctor/updateDoctor
-doctorRoute.route("/updateDoctor/:id").get((req,res)=>{
-doctorSchema.find(mongoose.Types.ObjectId(req.params.id),(err,data)=>{
-    if(err)
-        return err;
-    else
-        res.json(data)
-})
-}).put((req,res)=>{
-    doctorSchema.findByIdAndUpdate(mongoose.Types.ObjectId(req.params.id),{$set: req.body},(err,data)=>{
-        if(err)
-        return err;
-    else
-        res.json(data)
-    })
-})
+// http://localhost:4000/doctor/updateDoctor
+doctorRoute.route("/updateDoctor/:id").get((req, res) => {
+  doctorSchema.find(mongoose.Types.ObjectId(req.params.id), (err, data) => {
+    if (err) return err;
+    else res.json(data);
+  });
+}).put((req, res) => {
+  doctorSchema.findByIdAndUpdate(
+    mongoose.Types.ObjectId(req.params.id),
+    { $set: req.body },
+    (err, data) => {
+      if (err) return err;
+      else res.json(data);
+    }
+  );
+});
 
-//http://localhost:4000/doctor/deleteDoctor/:id
-doctorRoute.delete("/deleteDoctor/:id",(req,res)=>{
-    doctorSchema.findByIdAndRemove(mongoose.Types.ObjectId(req.params.id),(err,data)=>{
-        if(err)
-        return err;
-    else
-        res.json(data)
-    })
-})
-module.exports=doctorRoute;
+// http://localhost:4000/doctor/deleteDoctor/:id
+doctorRoute.delete("/deleteDoctor/:id", (req, res) => {
+  doctorSchema.findByIdAndRemove(
+    mongoose.Types.ObjectId(req.params.id),
+    (err, data) => {
+      if (err) return err;
+      else res.json(data);
+    }
+  );
+});
+
+// http://localhost:4000/doctor/specialties
+doctorRoute.get("/specialties", async (req, res) => {
+  try {
+    const specialties = await doctorSchema.distinct("specialization");
+    res.json(specialties);
+  } catch (error) {
+    console.error("Error fetching specialties:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// http://localhost:4000/doctor/specialty/:specialty
+doctorRoute.get("/specialty/:specialty", async (req, res) => {
+  try {
+    const doctors = await doctorSchema.find({ specialization: req.params.specialty }, "doctorName");
+    res.json(doctors.map((doctor) => doctor.doctorName));
+  } catch (error) {
+    console.error("Error fetching doctors by specialty:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// http://localhost:4000/doctor/availability/:doctor/:date
+doctorRoute.get("/availability/:doctor/:date", async (req, res) => {
+  try {
+    const doctor = await doctorSchema.findOne({ doctorName: req.params.doctor });
+    // Assuming the doctor schema has an 'availability' field
+    const availabilityData = {
+      bookedSlots: doctor.availability?.[req.params.date]?.bookedSlots || [],
+      availableSlots: doctor.availability?.[req.params.date]?.availableSlots || [],
+    };
+    res.json(availabilityData);
+  } catch (error) {
+    console.error("Error fetching doctor availability:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+doctorRoute.get("/availability/:doctor/:date", async (req, res) => {
+    try {
+      const doctor = await doctorSchema.findOne({ doctorName: req.params.doctor });
+      // Assuming the doctor schema has an 'availability' field
+      const availabilityData = doctor.availability?.[req.params.date] || { bookedSlots: [], availableSlots: [] };
+      res.json(availabilityData);
+    } catch (error) {
+      console.error("Error fetching doctor availability:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+module.exports = doctorRoute;
--- model/doctorSchema.js
+++ model/doctorSchema.js
@@ -1,13 +1,14 @@
-const mongoose=require("mongoose");
-const doctorSchema=new mongoose.Schema({
-    "doctorName":{type:String},
-    "doctorId":{type:Number},
-    "specialization":{type:String},
-    "qualification":{type:String},
-    "password":{type:String}
+const mongoose = require("mongoose");
+
+const doctorSchema = new mongoose.Schema({
+  doctorName: { type: String },
+  doctorId: { type: Number },
+  specialization: { type: String },
+  qualification: { type: String },
+  password: { type: String },
+  availability: { type: [String], default: [] }, // Array of available time slots
 },{
-    collection:"doctor"
-}
-)
-module.exports=mongoose.model("doctorSchema",doctorSchema);
-//hello
+    collection: "doctor"
+});
+
+module.exports = mongoose.model("doctorSchema", doctorSchema);
