bc9c8485e797029b8bfd3287ba4435cb06c74457
NaveedHafeez004 <144110314+NaveedHafeez004@users.noreply.github.com>
2023-11-15 19:17:18 +0530

    rjijr

--- model/doctorSchema.js
+++ model/doctorSchema.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-
-const doctorSchema = new mongoose.Schema({
-  doctorName: { type: String },
-  doctorId: { type: Number },
-  specialization: { type: String },
-  qualification: { type: String },
-  password: { type: String },
-  availability: { type: [String], default: [] }, // Array of available time slots
-},{
-    collection: "doctor"
-});
-
-module.exports = mongoose.model("doctorSchema", doctorSchema);
-=======
 const mongoose=require("mongoose");
 const doctorSchema=new mongoose.Schema({
     "doctorName":{type:String},
